fc66af6d6e3fa4b9ae9719f8b683444e65c92a8f
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-08-30 11:51:49 +0100

    Updates
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- clean.js
+++ clean.js
@@ -3,6 +3,12 @@ const github = require('@actions/github');
 
 try {
  console.log(`Cleaned up the action`)
+
+  await core.summary
+  .addHeading('Cleanup')
+  .addRaw("This cleanup has completed in the post action step.")
+  .write()
+
 } catch (error) {
   core.setFailed(error.message);
 }
