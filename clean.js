ee7c514bd57f9f622d9ae25ff16d6dd446a473c2
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2023-08-30 11:52:52 +0100

    Async run
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- clean.js
+++ clean.js
@@ -1,15 +1,18 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-try {
- console.log(`Cleaned up the action`)
+async function run() {
+  try {
+  console.log(`Cleaned up the action`)
 
-  await core.summary
-  .addHeading('Cleanup')
-  .addRaw("This cleanup has completed in the post action step.")
-  .write()
+    await core.summary
+    .addHeading('Cleanup')
+    .addRaw("This cleanup has completed in the post action step.")
+    .write()
 
-} catch (error) {
-  core.setFailed(error.message);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
 }
 
+run()
